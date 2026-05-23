3d978eac63ea3c7526ddc55caaaa2ff63b59e012
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-05-23 16:21:54 +0700

    Add Google site verification meta

--- wp-content/themes/custom-box-theme/header.php
+++ wp-content/themes/custom-box-theme/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="LOs4ohXuZmpUmZKsNAkORmduEB6kRpSOhTVwpx6FjLI">
 
     <script type="text/javascript">
         (function(c,l,a,r,i,t,y){
