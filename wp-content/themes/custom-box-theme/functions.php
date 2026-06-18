dd8ba18222e388a796fd0deb52ae7c10ea35b84a
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-06-18 16:26:09 +0700

    Add cardboard wine cartons post sync

--- wp-content/themes/custom-box-theme/functions.php
+++ wp-content/themes/custom-box-theme/functions.php
@@ -23,6 +23,7 @@ $custom_box_inc_files = array(
     'inc/paper-thickness-post-sync.php',
     'inc/paper-box-durability-post-sync.php',
     'inc/logo-printing-post-sync.php',
+    'inc/cardboard-wine-cartons-materials-uses-post-sync.php',
     'inc/obsolete-packaging-materials-page.php',
     'inc/supplier-audit-post-sync.php',
     'inc/admin-category-fields.php',
