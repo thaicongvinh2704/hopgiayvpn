3dc4ff28def539966db43424df268c4895974dc0
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-07-15 11:03:40 +0700

    Limit post sync loading in admin

--- wp-content/themes/custom-box-theme/functions.php
+++ wp-content/themes/custom-box-theme/functions.php
@@ -22,6 +22,34 @@ $custom_box_inc_files = array(
     'inc/admin-unused-product-category-cleanup.php',
     'inc/admin-product-sample-deploy.php',
     'inc/custom-vial-box-product-sync.php',
+    'inc/quote-form-handler.php',
+    'inc/woocommerce.php',
+);
+
+foreach ($custom_box_inc_files as $custom_box_inc_file) {
+    require_once get_template_directory() . '/' . $custom_box_inc_file;
+}
+
+function custom_box_should_load_post_sync_files(): bool
+{
+    if (!is_admin()) {
+        return false;
+    }
+
+    if ((function_exists('wp_doing_ajax') && wp_doing_ajax()) || (defined('REST_REQUEST') && REST_REQUEST) || (defined('DOING_CRON') && DOING_CRON)) {
+        return false;
+    }
+
+    if (isset($_GET['custom_box_run_post_syncs']) && '1' === sanitize_text_field(wp_unslash($_GET['custom_box_run_post_syncs']))) {
+        return true;
+    }
+
+    $pagenow = isset($GLOBALS['pagenow']) ? (string) $GLOBALS['pagenow'] : '';
+
+    return in_array($pagenow, array('index.php', 'tools.php'), true);
+}
+
+$custom_box_post_sync_files = array(
     'inc/cosmetic-paper-packaging-design-post-sync.php',
     'inc/skincare-paper-packaging-selection-post-sync.php',
     'inc/food-paper-packaging-selection-post-sync.php',
@@ -33,10 +61,10 @@ $custom_box_inc_files = array(
     'inc/how-to-package-chocolate-gift-sets-post-sync.php',
     'inc/how-to-create-premium-food-packaging-with-paper-boxes-post-sync.php',
     'inc/how-to-protect-bottles-in-paper-gift-packaging-post-sync.php',
-    'inc/quote-form-handler.php',
-    'inc/woocommerce.php',
 );
 
-foreach ($custom_box_inc_files as $custom_box_inc_file) {
-    require_once get_template_directory() . '/' . $custom_box_inc_file;
+if (custom_box_should_load_post_sync_files()) {
+    foreach ($custom_box_post_sync_files as $custom_box_post_sync_file) {
+        require_once get_template_directory() . '/' . $custom_box_post_sync_file;
+    }
 }
