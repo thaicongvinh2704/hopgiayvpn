81934c644e0cda359ea0d40e50499962f4e7c1f8
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-07-17 11:55:17 +0700

    Show synced category thumbnails on homepage

--- wp-content/themes/custom-box-theme/template-parts/home/packaging-category-groups.php
+++ wp-content/themes/custom-box-theme/template-parts/home/packaging-category-groups.php
@@ -36,10 +36,21 @@ $category_groups = function_exists('custom_box_get_home_packaging_category_group
                     <?php continue; ?>
                 <?php endif; ?>
                 <?php foreach ($category_group['items'] as $category_item) : ?>
+                    <?php
+                    $category_image_url = !empty($category_item[2]) ? $category_item[2] : '';
+
+                    if (!$category_image_url && function_exists('custom_box_get_product_category_card_image_url')) {
+                        $category_term = get_term_by('slug', $category_item[1], 'product_cat');
+
+                        if ($category_term && !is_wp_error($category_term)) {
+                            $category_image_url = custom_box_get_product_category_card_image_url($category_term, 'medium_large');
+                        }
+                    }
+                    ?>
                     <a class="home-packaging-category-card" href="<?php echo esc_url($resolve_category_url($category_item[1])); ?>">
-                        <span class="home-packaging-category-image <?php echo empty($category_item[2]) ? 'is-empty' : ''; ?>">
-                            <?php if (!empty($category_item[2])) : ?>
-                                <img src="<?php echo esc_url($category_item[2]); ?>" alt="<?php echo esc_attr($category_item[0]); ?>" loading="lazy" decoding="async">
+                        <span class="home-packaging-category-image <?php echo empty($category_image_url) ? 'is-empty' : ''; ?>">
+                            <?php if ($category_image_url) : ?>
+                                <img src="<?php echo esc_url($category_image_url); ?>" alt="<?php echo esc_attr($category_item[0]); ?>" loading="lazy" decoding="async">
                             <?php endif; ?>
                         </span>
                         <span class="home-packaging-category-title"><?php echo esc_html($category_item[0]); ?></span>
