4237b27f86fe01d303b6358c0dfff5af8fbaa2ba
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-08-15 14:03:53 +0700

    Import recycled kraft paper roll buying guide

--- wp-content/themes/custom-box-theme/inc/post-sync-loader.php
+++ wp-content/themes/custom-box-theme/inc/post-sync-loader.php
@@ -31,6 +31,11 @@ function custom_box_post_sync_registry(): array
             'option' => 'custom_box_why_vpn_paper_box_manufacturer_vietnam_sync_version',
             'slug' => 'why-vpn-paper-box-manufacturer-vietnam',
         ),
+        'inc/light-brown-recycled-kraft-paper-roll-post-sync.php' => array(
+            'version' => '2026-08-15-light-brown-recycled-kraft-paper-roll-v2',
+            'option' => 'custom_box_light_brown_recycled_kraft_paper_roll_sync_version',
+            'slug' => 'light-brown-recycled-kraft-paper-roll',
+        ),
         'inc/how-good-product-packaging-helps-business-grow-post-sync.php' => array(
             'version' => '2026-08-11-v1',
             'option' => 'custom_box_good_packaging_growth_sync_version',
