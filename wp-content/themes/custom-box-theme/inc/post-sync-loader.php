a7bc68d7a1f140081bf813fb6e64b1531c9539d7
thaicongvinh27 <thaiicongvinh@gmail.com>
2026-08-15 13:17:25 +0700

    Replace blurry supplier guide images

--- wp-content/themes/custom-box-theme/inc/post-sync-loader.php
+++ wp-content/themes/custom-box-theme/inc/post-sync-loader.php
@@ -27,7 +27,7 @@ function custom_box_post_sync_registry(): array
             'slug' => 'tissue-paper-for-gift-bags',
         ),
         'inc/why-vpn-paper-box-manufacturer-vietnam-post-sync.php' => array(
-            'version' => '2026-08-15-vpn-paper-box-manufacturer-v2',
+            'version' => '2026-08-15-vpn-paper-box-manufacturer-v3',
             'option' => 'custom_box_why_vpn_paper_box_manufacturer_vietnam_sync_version',
             'slug' => 'why-vpn-paper-box-manufacturer-vietnam',
         ),
